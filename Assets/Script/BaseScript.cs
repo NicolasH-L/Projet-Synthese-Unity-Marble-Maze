4ea7796c75af25bbaff9141e500faaf8cd0a4742
NicolasH-L <nhonglao@gmail.com>
2022-02-05 20:35:35 -0500

    Ajout d'un script qui permet de faire basculer la base

---
+++ Assets/Script/BaseScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class BaseScript : MonoBehaviour
+    {
+        private const float Smooth = 5.0f;
+        private const float TiltAngle = 15.0f;
+        
+        void Update()
+        {
+            float tiltAxeZ = Input.GetAxis("Horizontal") * TiltAngle;
+            float tiltAxeX = Input.GetAxis("Vertical") * TiltAngle;
+            
+            Quaternion target = Quaternion.Euler(tiltAxeX, 0, tiltAxeZ);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * Smooth);
+        }
+    }
+}
