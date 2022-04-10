7d13ac3b26f4c419a5d64812087dbdc527f80b98
NicolasH-L <nhonglao@gmail.com>
2022-04-10 18:31:45 -0400

    Script: Mise a jour des configurations

--- Assets/Script/BaseScript2.cs
+++ Assets/Script/BaseScript2.cs
@@ -5,15 +5,15 @@ namespace Script
     public class BaseScript2 : MonoBehaviour
     {
         private const float Smooth = 5.0f;
-        private const float TiltAngle = 5.0f;
-        
+        private const float TiltAngle = 6.0f;
+
         void Update()
         {
             float tiltAxeZ = Input.GetAxis("Horizontal2") * TiltAngle;
             float tiltAxeX = Input.GetAxis("Vertical2") * TiltAngle;
-            
-            Quaternion target = Quaternion.Euler(tiltAxeX, 0, tiltAxeZ);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * Smooth);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(tiltAxeX, 0, -tiltAxeZ),
+                Time.deltaTime * Smooth);
         }
     }
 }
